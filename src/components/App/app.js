8320ee4772a35d877bc6827458ee8a132ca9d761
noexanu <solonovichyan@gmail.com>
2021-03-21 12:19:50 +0100

    refactor: cleared & created new architecture

---
+++ src/components/App/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './app.scss';
+
+class App extends React.Component {
+  state = {
+  };
+  render() {
+    return (
+      <>
+        My App
+      </>
+    );
+  }
+}
+
+export default App;
